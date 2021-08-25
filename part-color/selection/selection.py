fa82a0304d2f65b6aa1eb94dc4b3ae49ab008746
kaaatsu32329 <katsu1112uchi@gmail.com>
2021-08-26 01:13:34 +0900

    Edit some variable.

--- part-color/selection/selection.py
+++ part-color/selection/selection.py
@@ -9,30 +9,30 @@ class Selection():
 
     def select(self):
         while True:
-            object = input('[I]mage or [M]ovie? >> ')
-            if object == 'I' or object == 'Image':
-                dir = '../sample_images/'
-                object = 'Image'
+            self.object = input('[I]mage or [M]ovie? >> ')
+            if self.object == 'I' or self.object == 'Image':
+                DIR = '../sample_images/'
+                self.object = 'Image'
                 break
-            elif object == 'M' or object == 'Movie':
-                dir = '../sample_movies/'
-                object = 'Movie'
+            elif self.object == 'M' or self.object == 'Movie':
+                DIR = '../sample_movies/'
+                self.object = 'Movie'
                 break
             else:
                 pass
-        list_datas = os.listdir(dir)
+        list_datas = os.listdir(DIR)
         print(list_datas)
-        target = input('Which do you want to process? >> ')
-        target = str(target)
-        return target, object
+        self.target = input('Which do you want to process? >> ')
+        target = str(self.target)
+        return self.target, self.object
 
     def select_color(self):
         print('Which color will you use?')
-        selected = input('red, green, blue, orange >> ')
-        selected = str(selected)
-        inverse = input('Inversion? [y]es/[n]o >> ')
-        if inverse == 'y' or inverse == 'yes':
-            inverse = True
+        self.selected = input('red, green, blue, orange >> ')
+        self.selected = str(self.selected)
+        self.inverse = input('Inversion? [y]es/[n]o >> ')
+        if self.inverse == 'y' or self.inverse == 'yes':
+            self.inverse = True
         else:
-            inverse = False
-        return selected, inverse
+            self.inverse = False
+        return self.selected, self.inverse
