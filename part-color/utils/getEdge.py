197f581e45c34e0b963748953a52549454412b48
kaaatsu32329 <katsu1112uchi@gmail.com>
2021-08-27 16:51:39 +0900

    Add function to get edge.

--- part-color/utils/getEdge.py
+++ part-color/utils/getEdge.py
@@ -1,3 +1,8 @@
-class GetEdge():
-    def __init__():
-        pass
+import cv2
+
+def get_edge(image):
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #gauss_image = cv2.GaussianBlur(gray_image, (3,3), 3)
+    #lap_image = cv2.Laplacian(gauss_image, cv2.CV_32F)
+    canny_image = cv2.Canny(gray_image, 100, 200)
+    return canny_image
