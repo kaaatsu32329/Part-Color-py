2ddfe57d5f8a988138d16b00d6194377f101ac21
kaaatsu32329 <katsu1112uchi@gmail.com>
2021-09-01 01:48:09 +0900

    Make Process class.

--- part-color/main.py
+++ part-color/main.py
@@ -1,37 +1,11 @@
-import sys
-from utils.selection import Selection
-from utils.colorBase import ColorBase
-from utils.objectBase import ObjectBase
-
-args = sys.argv
+from utils.process import Process
 
 def main():
+    process = Process()
     proceed = False
 
     while not proceed:
-        selection = Selection()
-        colorBase = ColorBase('test')
-        objectBase = ObjectBase('test')
-        target, object, base = selection.select()
-
-        if base == 'Color':
-            color, inverse = selection.select_color()
-            if object == 'Image':
-                colorBase.image_process(target, color, inverse)
-                proceed = True
-            elif object == 'Movie':
-                colorBase.movie_process(target, color, inverse)
-                proceed = True
-        elif base == 'Object':
-            object, inverse = selection.selected_object()
-            if object == 'Image':
-                objectBase.image_process(target, object, inverse)
-                proceed = True
-            elif object == 'Movie':
-                objectBase.movie_process(target, object, inverse)
-                proceed = True
-        else:
-            print('An error has occured!')
+        process.run()
 
 if __name__ == "__main__":
     main()
---
+++ part-color/utils/process.py
@@ -0,0 +1,32 @@
+from utils.selection import Selection
+from utils.colorBase import ColorBase
+from utils.objectBase import ObjectBase
+
+class Process():
+    def __init__(self, proceed):
+        self.proceed = proceed
+
+    def run(self, proceed):
+        selection = Selection()
+        colorBase = ColorBase('test')
+        objectBase = ObjectBase('test')
+        target, object, base = selection.select()
+
+        if base == 'Color':
+            color, inverse = selection.select_color()
+            if object == 'Image':
+                colorBase.image_process(target, color, inverse)
+                self.proceed = True
+            elif object == 'Movie':
+                colorBase.movie_process(target, color, inverse)
+                self.proceed = True
+        elif base == 'Object':
+            object, inverse = selection.selected_object()
+            if object == 'Image':
+                objectBase.image_process(target, object, inverse)
+                self.proceed = True
+            elif object == 'Movie':
+                objectBase.movie_process(target, object, inverse)
+                self.proceed = True
+        else:
+            print('An error has occured!')
